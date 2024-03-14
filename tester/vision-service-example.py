048594884d17747d2f07c0c0188c1048ade35507
karinegmg <kgmg@cin.ufpe.br>
2024-03-14 12:47:09 -0300

    feat: :sparkles: Add initial tester implementation with a single service

---
+++ tester/vision-service-example.py
@@ -0,0 +1,14 @@
+import zmq
+
+context = zmq.Context()
+sub = context.socket(zmq.SUB)
+address = "ipc:///tmp/vision-service"
+sub.connect(address)
+sub.setsockopt_string(zmq.SUBSCRIBE, "robocin")
+
+count = 0
+while True:
+
+    topic, message = sub.recv_multipart()
+    count = count + 1
+    print(f"Received request: {message} from {topic} for {count} times")
