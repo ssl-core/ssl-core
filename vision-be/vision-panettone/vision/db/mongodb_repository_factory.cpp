5408ad612e1f14fa5bcacb3fcdb1e2cceba40685
Matheus Andrade <matheusvncs160@gmail.com>
2024-04-11 21:42:20 -0300

    [WIP] Test mongo replica set with only vision service

--- vision-be/vision-panettone/vision/db/mongodb_repository_factory.cpp
+++ vision-be/vision-panettone/vision/db/mongodb_repository_factory.cpp
@@ -6,7 +6,7 @@ namespace vision {
 
 std::unique_ptr<IFrameRepository> MongoDbRepositoryFactory::createFrameRepository() const {
   return std::make_unique<FrameRepositoryMongoDb>(MongoDbRepositoryBuildArgs{
-      .uri = "mongodb://ssl-core-mongodb1-1:27017,ssl-core-mongodb2-1:27018,ssl-core-mongodb3-1:27019/?replicaSet=rs0",
+      .uri = "mongodb://mongodb1:27017,mongodb2:27018,mongodb3:27019/?replicaSet=rs0",
       .db_name = "frames_database",
       .collection_name = "frames_collection",
   });
