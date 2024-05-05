7a99a787b0f306fa74ac4050c7b07cebfe6a89f8
José Cruz <jvsc@cin.ufpe.br>
2024-05-05 18:23:35 -0300

    Add print library as example

--- embedded/nucleo-f767zi/example/example_main.cpp
+++ embedded/nucleo-f767zi/example/example_main.cpp
@@ -1,21 +1,7 @@
+#include "example/print.h"
 #include "mbed.h" // IWYU pragma: keep
 
-#include <format>
-
-// https://clangd.llvm.org/troubleshooting#cant-find-standard-library-headers-map-stdioh-etc
-// https://os.mbed.com/cookbook/mbed-cmake
-// https://github.com/ARMmbed/mbed-os/blob/master/tools/cmake/README.md
-
-// mbed-tools new .
-// mbed-tools configure -m nucleo_f767zi -t GCC_ARM
-
-// mbed-tools deploy
-// mbed-tools configure -m nucleo_f767zi -t GCC_ARM
-
-// sudo rsync -av --remove-source-files arm-none-eabi/ /usr/local/
-
 int main() {
-  std::puts(std::format("Hello, {}!", "Mbed").c_str());
-
+  example::println("Hello, {}-{}!", "nucleo", "f767zi");
   return 0;
 }
---
+++ embedded/nucleo-f767zi/example/print.h
@@ -0,0 +1,52 @@
+#ifndef EXAMPLE_PRINT_H
+#define EXAMPLE_PRINT_H
+
+#include <version>
+
+#if defined(__cpp_lib_print) and __cpp_lib_print >= 202207L
+
+#include <print>
+
+namespace robocin {
+
+using [[deprecated("use 'std::print' instead.")]] std::print;
+using [[deprecated("use 'std::println' instead.")]] std::println;
+
+} // namespace robocin
+
+#warning "Replace all robocin::print[ln] occurrences with std::print[ln] and remove this file."
+
+#else
+
+#include <cstdio>
+#include <format>
+
+namespace example {
+
+template <class... Args>
+void print(std::FILE* stream, std::format_string<Args...> fmt, Args&&... args) {
+  std::fputs(std::format(fmt, std::forward<Args>(args)...).c_str(), stream);
+}
+
+template <class... Args>
+void print(std::format_string<Args...> fmt, Args&&... args) {
+  std::fputs(std::format(fmt, std::forward<Args>(args)...).c_str(), stdout);
+}
+
+template <class... Args>
+void println(std::FILE* stream, std::format_string<Args...> fmt, Args&&... args) {
+  print(stream, fmt, std::forward<Args>(args)...);
+  std::fputc('\n', stream);
+}
+
+template <class... Args>
+void println(std::format_string<Args...> fmt, Args&&... args) {
+  print(stdout, fmt, std::forward<Args>(args)...);
+  std::fputc('\n', stdout);
+}
+
+} // namespace example
+
+#endif
+
+#endif // EXAMPLE_PRINT_H
