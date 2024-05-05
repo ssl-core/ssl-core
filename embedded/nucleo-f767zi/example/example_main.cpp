036f6a3b956b136a26224202f2d88a91b577eeaa
José Cruz <jvsc@cin.ufpe.br>
2024-05-05 13:06:15 -0300

    Update example_main.cpp

--- embedded/nucleo-f767zi/example/example_main.cpp
+++ embedded/nucleo-f767zi/example/example_main.cpp
@@ -1,4 +1,6 @@
-#include "mbed.h"
+#include "mbed.h" // IWYU pragma: keep
+
+#include <format>
 
 // https://clangd.llvm.org/troubleshooting#cant-find-standard-library-headers-map-stdioh-etc
 // https://os.mbed.com/cookbook/mbed-cmake
@@ -10,8 +12,10 @@
 // mbed-tools deploy
 // mbed-tools configure -m nucleo_f767zi -t GCC_ARM
 
+// sudo rsync -av --remove-source-files arm-none-eabi/ /usr/local/
+
 int main() {
-  printf("Hello, Mbed!\n");
+  std::puts(std::format("Hello, {}!", "Mbed").c_str());
 
   return 0;
 }
