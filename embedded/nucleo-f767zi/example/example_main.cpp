21759da9e20e69b1d081f0271b297a53c9438f14
José Cruz <jvsc@cin.ufpe.br>
2024-05-12 19:58:10 -0300

    Update embedded/nucleo-f767zi example

--- embedded/nucleo-f767zi/example/example_main.cpp
+++ embedded/nucleo-f767zi/example/example_main.cpp
@@ -1,5 +1,11 @@
+#include "example/example.nanopb.h"
 #include "example/print.h"
 #include "mbed.h" // IWYU pragma: keep
+#include "pb_common.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+
+#include <version>
 
 // mbed-tools detect // show boards
 // mbed-tools sterm // open serial terminal
@@ -7,7 +13,21 @@
 // inside bin directory:
 // mbedflash simple -i ${BINARY} --target_id ${TARGET_ID} // flash
 
+// pip install --upgrade google-api-python-client
+
 int main() {
   robocin::println("Hello, {}-{}!", "nucleo", "f767zi");
+
+  example_ExampleNanoPb2 example = {
+      .integer = 10,
+      .text = "jose",
+  };
+
+  std::array<uint8_t, 256> buffer{};
+  pb_ostream_t stream = pb_ostream_from_buffer(buffer.data(), sizeof(buffer));
+  pb_encode(&stream, example_ExampleNanoPb2_fields, &example);
+
+  std::string bytes(reinterpret_cast<const char*>(buffer.data()), stream.bytes_written);
+
   return 0;
 }
