0315a15a88eb9f04aff5e88d1e6cd6b37c3dfb95
José Cruz <jvsc@cin.ufpe.br>
2024-05-12 00:40:10 -0300

    Add robocin_cpp_nanopb_library and update 'find_package's

--- .cmake/essentials.cmake
+++ .cmake/essentials.cmake
@@ -54,7 +54,7 @@ endif ()
 # Find abseil installation
 # It enable the following variables:
 #   absl::...                             the abseil libraries (https://abseil.io/docs/cpp/guides)
-find_package(absl CONFIG QUIET HINTS "/usr/local/absl" "/opt/absl" "/usr/local/protobuf" "/opt/protobuf")
+find_package(absl CONFIG QUIET HINTS "/opt/absl" "/opt/protobuf")
 if (absl_FOUND)
   message(STATUS "Using absl: ${absl_DIR}")
 endif ()
@@ -70,7 +70,7 @@ endif ()
 
 # find GTest installation
 # looks for GTest cmake config files installed by GTest's cmake installation.
-find_package(GTest CONFIG QUIET HINTS "/usr/local/googletest" "/opt/googletest")
+find_package(GTest CONFIG QUIET HINTS "/opt/googletest")
 if (GTest_FOUND)
   message(STATUS "Using GTest: ${GTest_VERSION}")
 
@@ -88,7 +88,7 @@ endif ()
 
 # find Google Benchmark installation
 # looks for Google Benchmark cmake config files installed by Google Benchmark's cmake installation.
-find_package(benchmark CONFIG QUIET HINTS "/usr/local/benchmark" "/opt/benchmark")
+find_package(benchmark CONFIG QUIET HINTS "/opt/benchmark")
 if (benchmark_FOUND)
   message(STATUS "Using Google Benchmark: ${benchmark_VERSION}")
 endif ()
@@ -100,14 +100,26 @@ endif ()
 # It enable the following variables:
 #   protobuf::libprotobuf                 the protobuf library
 #   $<TARGET_FILE:protobuf::protoc>       the protobuf compiler
-find_package(utf8_range CONFIG QUIET HINTS "/usr/local/protobuf" "/opt/protobuf") # protobuf dependency.
+find_package(utf8_range CONFIG QUIET HINTS "/opt/protobuf") # protobuf dependency.
 if (utf8_range_FOUND)
   message(STATUS "Using utf8_range: ${utf8_range_DIR}")
 endif ()
 
-find_package(Protobuf CONFIG QUIET HINTS "/usr/local/protobuf" "/opt/protobuf")
+find_package(Protobuf CONFIG QUIET HINTS "/opt/protobuf")
 if (Protobuf_FOUND)
   message(STATUS "Using Protobuf: ${Protobuf_VERSION}")
+endif ()
+
+########################################################################################################################
+
+# Find nanopb installation
+# Looks for nanopb cmake config files installed by nanopb's cmake installation.
+# It enable the following variable:
+#   nanopb::protobuf-nanopb-static                 the nanopb library
+find_package(nanopb CONFIG QUIET HINTS "/opt/nanopb")
+if (nanopb_FOUND)
+  find_program(NANOPB_GENERATOR NAMES nanopb_generator REQUIRED)
+  message(STATUS "Using nanopb: ${nanopb_VERSION} - nanopb_generator at: '${NANOPB_GENERATOR}'")
 endif ()
 
 ########################################################################################################################
@@ -573,5 +585,110 @@ function(robocin_cpp_proto_library)
   )
 
 endfunction(robocin_cpp_proto_library)
+
+########################################################################################################################
+
+# Add cpp proto library
+# Named parameters:
+#  NAME: name of the library
+#  PROTOS: proto files
+#  DEPS: dependencies
+#  MACROS: macros
+#  COMPILE_OPTIONS: compile options
+#  COMPILE_FEATURES: compile features
+function(robocin_cpp_nanopb_library)
+  cmake_parse_arguments(
+          ARG                                                     # prefix of output variables
+          ""                                                      # list of names of the boolean arguments
+          "NAME"                                                  # list of names of mono-valued arguments
+          "PROTOS;DEPS;MACROS;COMPILE_OPTIONS;COMPILE_FEATURES"   # list of names of multi-valued arguments
+          ${ARGN}                                                 # arguments of the function to parse
+  )
+
+  # if there isn't at least one proto file, then the library is not created
+  if (NOT ARG_PROTOS)
+    message(WARNING "robocin_cpp_nanopb_library: no proto files given for library '${ARG_NAME}'.")
+    return()
+  endif ()
+
+  set(proto_hdrs)
+  set(proto_srcs)
+
+  foreach (PROTO ${ARG_PROTOS})
+    get_filename_component(proto_name ${PROTO} NAME_WE)
+    get_filename_component(proto_absolute_file ${PROTO} ABSOLUTE)
+    get_filename_component(proto_absolute_path ${proto_absolute_file} DIRECTORY)
+    file(RELATIVE_PATH proto_relative_file ${ROBOCIN_PROJECT_PATH} ${proto_absolute_file})
+    file(RELATIVE_PATH proto_relative_path ${ROBOCIN_PROJECT_PATH} ${proto_absolute_path})
+
+    set(proto_hdr_file "${CMAKE_BINARY_DIR}/${proto_relative_path}/${proto_name}.pb.h")
+    set(proto_src_file "${CMAKE_BINARY_DIR}/${proto_relative_path}/${proto_name}.pb.c")
+
+    add_custom_command(
+            OUTPUT "${proto_hdr_file}" "${proto_src_file}"
+            COMMAND ${NANOPB_GENERATOR}
+            ARGS --proto-path ${ROBOCIN_PROJECT_PATH}
+            --output-dir "${CMAKE_BINARY_DIR}"
+            "${proto_relative_file}"
+            DEPENDS "${proto_absolute_file}"
+            WORKING_DIRECTORY ${ROBOCIN_PROJECT_PATH}
+    )
+
+    list(APPEND proto_hdrs ${proto_hdr_file})
+    list(APPEND proto_srcs ${proto_src_file})
+
+  endforeach (PROTO)
+
+  add_library(${ARG_NAME} ${proto_hdrs} ${proto_srcs})
+  target_link_libraries(${ARG_NAME} PUBLIC nanopb::protobuf-nanopb-static ${ARG_DEPS}) # link library with given dependencies
+
+  target_include_directories(${ARG_NAME} PRIVATE ${ROBOCIN_PROJECT_PATH})
+  target_include_directories(${ARG_NAME} PRIVATE ${CMAKE_BINARY_DIR})
+
+  if (ARG_MACROS)
+    target_compile_definitions(${ARG_NAME} ${ARG_MACROS})
+  endif ()
+
+  if (ARG_COMPILE_OPTIONS)
+    target_compile_options(${ARG_NAME} ${ARG_COMPILE_OPTIONS})
+  endif ()
+
+  # installing steps:
+  if (ROBOCIN_EMBEDDED_CROSS_COMPILING)
+    # installation is not allowed when cross compiling
+    return()
+  endif ()
+
+  # installing steps:
+  #  - include directories to be used by other projects
+  target_include_directories(${ARG_NAME} INTERFACE
+          $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+          $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+  )
+  #  - install header files preserving the directory structure
+  foreach (HDR_FILE ${proto_hdrs})
+    file(RELATIVE_PATH header_relative_path ${ROBOCIN_PROJECT_PATH} ${CMAKE_CURRENT_SOURCE_DIR})
+
+    get_filename_component(header_relative_subdirectory ${HDR_FILE} DIRECTORY)
+    file(RELATIVE_PATH header_relative_subdirectory ${CMAKE_CURRENT_BINARY_DIR} ${header_relative_subdirectory})
+
+    robocin_concatenate_paths("${header_relative_path}" "${header_relative_subdirectory}" header_relative_path)
+    robocin_concatenate_paths("${CMAKE_INSTALL_INCLUDEDIR}" "${header_relative_path}" header_install_path)
+
+    install(FILES ${HDR_FILE} DESTINATION "${header_install_path}")
+  endforeach ()
+
+  #  - install the project's library
+  install(TARGETS ${ARG_NAME} EXPORT "${PROJECT_NAME}Targets")
+
+  #  - install CMake configuration files
+  install(
+          EXPORT "${PROJECT_NAME}Targets"
+          NAMESPACE "${PROJECT_NAME}::"
+          FILE "${PROJECT_NAME}Config.cmake"
+          DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+  )
+
+endfunction(robocin_cpp_nanopb_library)
 
 ########################################################################################################################
