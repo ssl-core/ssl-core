f9b88c44247191bc628c7059834e19d3cc5c9e08
José Cruz <jvsc@cin.ufpe.br>
2024-05-12 14:04:48 -0300

    Update nanopb.sh script

--- containers/scripts/nanopb.sh
+++ containers/scripts/nanopb.sh
@@ -11,7 +11,15 @@ fi
 
 VERSION="${1}"
 PARENT_DIR="${2}"
+CMAKE_ADDITIONAL_ARGS="${3}"
 CURRENT_USER=$(who | awk 'NR==1{print $1}')
+
+CMAKE_ARGS=()
+CMAKE_ARGS+=("-DCMAKE_BUILD_TYPE=Release")
+CMAKE_ARGS+=("-DCMAKE_INSTALL_PREFIX=${NANOPB_DIR}")
+for arg in ${CMAKE_ADDITIONAL_ARGS[@]}; do
+    CMAKE_ARGS+=("$arg")
+done
 
 if [ -z "${VERSION}" ]; then
   echo -e "\x1B[31m[ERROR] No version specified."
@@ -27,6 +35,8 @@ TMP_NANOPB="/tmp/nanopb"
 
 echo -e "\x1B[01;93m\nInstalling or updating nanopb...\n\u001b[0m"
 
+pip install --upgrade google-api-python-client # required.
+
 rm -rf "${TMP_NANOPB}"
 mkdir -p "${TMP_NANOPB}"
 
@@ -35,10 +45,7 @@ tar -xvf "${TMP_NANOPB}/nanopb-${VERSION}.tar.gz" -C "${TMP_NANOPB}"
 rm -rf "${TMP_NANOPB}/nanopb-${VERSION}.tar.gz"
 
 pushd "${TMP_NANOPB}/nanopb-${VERSION}" || exit 1
-cmake -B build \
-      -S . \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="${NANOPB_DIR}"
+cmake -B build -S . ${CMAKE_ARGS}
 cmake --build build -j "$(nproc)"
 cmake --install build
 popd || exit 1
