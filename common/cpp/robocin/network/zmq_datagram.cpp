d894a059cc8d7b385617abca57cd2be8dd7a07cb
José Victor Cruz <jvsc@cin.ufpe.br>
2024-05-29 00:42:09 -0300

    Add missing #ifdef zmq exists guards to zmq_datagram.cpp

--- common/cpp/robocin/network/zmq_datagram.cpp
+++ common/cpp/robocin/network/zmq_datagram.cpp
@@ -1,5 +1,8 @@
 #include "robocin/network/zmq_datagram.h"
 
+#if defined(__robocin_lib_zmq) and __robocin_lib_zmq >= 202405L
+#if defined(__robocin_lib_cppzmq) and __robocin_lib_cppzmq >= 202405L
+
 namespace robocin {
 
 ZmqDatagram::ZmqDatagram(std::string_view topic, std::string_view message) : // NOLINT(*swappable*)
@@ -13,3 +16,6 @@ std::string_view ZmqDatagram::message() const { return message_; }
 bool ZmqDatagram::empty() const { return topic_.empty() && message_.empty(); }
 
 } // namespace robocin
+
+#endif
+#endif
