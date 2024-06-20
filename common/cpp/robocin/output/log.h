6f9a9b8c5230272c9b65e0b256c485fda8d0ee3a
José Victor Cruz <jvsc@cin.ufpe.br>
2024-06-19 21:58:28 -0300

    Update common/cpp/robocin/output/log.h

--- common/cpp/robocin/output/log.h
+++ common/cpp/robocin/output/log.h
@@ -3,67 +3,42 @@
 
 #include <cstdlib>
 #include <format>
-#include <utility>
+#include <print>
 
 namespace robocin {
-namespace log_internal {
-
-template <class... Args>
-void print(std::FILE* stream, std::format_string<Args...> fmt, Args&&... args) {
-  std::fputs(std::format(fmt, std::forward<Args>(args)...).c_str(), stream);
-}
-
-template <class... Args>
-void print(std::format_string<Args...> fmt, Args&&... args) {
-  std::fputs(std::format(fmt, std::forward<Args>(args)...).c_str(), stdout);
-}
-
-template <class... Args>
-void println(std::FILE* stream, std::format_string<Args...> fmt, Args&&... args) {
-  print(stream, fmt, std::forward<Args>(args)...);
-  std::fputc('\n', stream);
-}
-
-template <class... Args>
-void println(std::format_string<Args...> fmt, Args&&... args) {
-  print(stdout, fmt, std::forward<Args>(args)...);
-  std::fputc('\n', stdout);
-}
-
-} // namespace log_internal
 
 // TODO(#126): add source location and fill files with logs.
 
 template <class... Args>
 void ilog(std::format_string<Args...> fmt, Args&&... args) {
   //  auto file_name = std::string_view{loc.file_name()}.substr(sizeof(ROBOCIN_REPOSITORY_PATH));
-  //  log_internal::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
+  //  std::print(stdout, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
 
-  log_internal::println(stdout, fmt, std::forward<Args>(args)...);
+  std::println(stdout, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void wlog(std::format_string<Args...> fmt, Args&&... args) {
   //  auto file_name = std::string_view{loc.file_name()}.substr(sizeof(ROBOCIN_REPOSITORY_PATH));
-  //  log_internal::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
+  //  std::print(stdout, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
 
-  log_internal::println(stdout, fmt, std::forward<Args>(args)...);
+  std::println(stdout, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void elog(std::format_string<Args...> fmt, Args&&... args) {
   //  auto file_name = std::string_view{loc.file_name()}.substr(sizeof(ROBOCIN_REPOSITORY_PATH));
-  //  log_internal::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
+  //  std::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
 
-  log_internal::println(stderr, fmt, std::forward<Args>(args)...);
+  std::println(stderr, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void flog(std::format_string<Args...> fmt, Args&&... args) {
   //  auto file_name = std::string_view{loc.file_name()}.substr(sizeof(ROBOCIN_REPOSITORY_PATH));
-  //  log_internal::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
+  //  std::print(stderr, "[{}:{}:{}] ", file_name, loc.line(), loc.column());
 
-  log_internal::println(stderr, fmt, std::forward<Args>(args)...);
+  std::println(stderr, fmt, std::forward<Args>(args)...);
 
   std::abort();
 }
