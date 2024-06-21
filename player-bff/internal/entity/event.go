07535205f3083f0c43a385c43cf2781a04bf3497
Uanderson Ricardo <uandersonrfs@gmail.com>
2024-06-21 16:56:56 -0300

    Add Event entity implementation

--- player-bff/internal/entity/event.go
+++ player-bff/internal/entity/event.go
@@ -2,15 +2,29 @@ package entity
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/robocin/ssl-core/player-bff/pkg/pb/common"
 )
 
 type Event struct {
+	Type    string
+	Data    interface{}
+	Sources []string
+	Time    time.Time
 }
 
 func NewEvent(gameEvent *common.GameEvent) Event {
-	return Event{}
+	reflector := gameEvent.ProtoReflect()
+	descriptor := reflector.Descriptor()
+	event := reflector.WhichOneof(descriptor.Oneofs().ByName("Event"))
+
+	return Event{
+		Type:    event.TextName(),
+		Data:    event.Message().Fields(),
+		Sources: gameEvent.Sources,
+		Time:    gameEvent.Timestamp.AsTime(),
+	}
 }
 
 func (c *Event) ToJson() ([]byte, error) {
