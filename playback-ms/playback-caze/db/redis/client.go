fd068c6a7085671512aca3f99390c087490690a9
Matheus Teotonio <matheusvncs160@gmail.com>
2024-07-13 09:22:03 -0300

    RedisClient#convertTimeToKey to use same time resolution

--- playback-ms/playback-caze/db/redis/client.go
+++ playback-ms/playback-caze/db/redis/client.go
@@ -41,25 +41,30 @@ func (rc *RedisClient) Set(key time.Time, value interface{}) error {
 	ctx := context.Background()
 	args := &redis.XAddArgs{
 		Stream: rc.stream,
-		ID:     fmt.Sprintf("%d", key.UnixMilli()),
+		ID:     rc.convertTimeToKey(key),
 		Values: map[string]interface{}{
 			"value": value,
 		},
 	}
+	fmt.Printf("Setting key %s with value %v...\n", rc.convertTimeToKey(key), value)
 	return rc.client.XAdd(ctx, args).Err()
 }
 
 func (rc *RedisClient) Get(key time.Time) (interface{}, error) {
 	ctx := context.Background()
-	return rc.client.XRangeN(ctx, rc.stream, fmt.Sprintf("%d", key.UnixMilli()), "+", 1).Result()
+	return rc.client.XRangeN(ctx, rc.stream, rc.convertTimeToKey(key), "+", 1).Result()
 }
 
 func (rc *RedisClient) GetChunk(start_time, end_time time.Time) (interface{}, error) {
 	ctx := context.Background()
-	return rc.client.XRange(ctx, rc.stream, fmt.Sprintf("%d", start_time.UnixMilli()), fmt.Sprintf("%d", end_time.UnixMilli())).Result()
+	return rc.client.XRange(ctx, rc.stream, rc.convertTimeToKey(start_time), rc.convertTimeToKey(end_time)).Result()
 }
 
 func (rc *RedisClient) Clear() error {
 	ctx := context.Background()
 	return rc.client.FlushAll(ctx).Err()
+}
+
+func (rc *RedisClient) convertTimeToKey(t time.Time) string {
+	return fmt.Sprintf("%d", t.UnixNano())
 }
