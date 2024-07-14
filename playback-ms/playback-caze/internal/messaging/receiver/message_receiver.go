a45bd2e5a32acd47fdf9b83835808ca163aba532
José Victor Cruz <jvsc@cin.ufpe.br>
2024-07-13 21:12:56 -0300

    Add zmq poller to playback-ms

--- playback-ms/playback-caze/internal/messaging/receiver/message_receiver.go
+++ playback-ms/playback-caze/internal/messaging/receiver/message_receiver.go
@@ -5,32 +5,65 @@ import (
 
 	"github.com/robocin/ssl-core/playback-ms/internal/concurrency"
 	"github.com/robocin/ssl-core/playback-ms/network"
+	"github.com/zeromq/goczmq"
+)
+
+const (
+	pollerTimeout = 10 /*ms ~= 100hz*/
 )
 
 type MessageReceiver struct {
 	subscribers []*network.ZmqSubscriberSocket
+	poller      *goczmq.Poller
 }
 
 func NewMessageReceiver(sockets []*network.ZmqSubscriberSocket) *MessageReceiver {
+	poller, err := goczmq.NewPoller()
+
+	if err != nil {
+		panic("error creating zmq poller")
+	}
+
+	for i := range sockets {
+		poller.Add(sockets[i].Socket)
+	}
+
 	return &MessageReceiver{
 		subscribers: sockets,
+		poller:      poller,
 	}
 }
 
 func (mr *MessageReceiver) Start(datagrams *concurrency.ConcurrentQueue[network.ZmqMultipartDatagram], wg *sync.WaitGroup) {
-	for _, sub := range mr.subscribers {
-		wg.Add(1)
+	wg.Add(1)
 
-		go func() {
-			defer wg.Done()
+	go func() {
+		defer wg.Done()
+
+		for {
+			socket := mr.poller.Wait(pollerTimeout)
+			if socket == nil {
+				continue
+			}
 
 			for {
-				datagram := sub.Receive()
+				datagram := receiveFrom(socket)
 				if datagram.IsEmpty() {
-					continue
+					break
 				}
+
 				datagrams.Enqueue(datagram)
 			}
-		}()
+		}
+	}()
+}
+
+func receiveFrom(socket *goczmq.Sock) network.ZmqMultipartDatagram {
+	bytes, err := socket.RecvMessageNoWait()
+
+	if len(bytes) == 0 || err != nil {
+		return network.ZmqMultipartDatagram{}
 	}
+
+	return network.NewZmqMultipartDatagram(bytes[0], bytes[1])
 }
--- playback-ms/playback-caze/network/zmq_multipart_datagram.go
+++ playback-ms/playback-caze/network/zmq_multipart_datagram.go
@@ -5,13 +5,13 @@ type ZmqMultipartDatagram struct {
 	Message    []byte
 }
 
-func NewZmqMultipartDatagram(identity []byte, message []byte) *ZmqMultipartDatagram {
-	return &ZmqMultipartDatagram{
+func NewZmqMultipartDatagram(identity []byte, message []byte) ZmqMultipartDatagram {
+	return ZmqMultipartDatagram{
 		Identifier: identity,
 		Message:    message,
 	}
 }
 
 func (zmd *ZmqMultipartDatagram) IsEmpty() bool {
-	return zmd.Identifier == nil || zmd.Message == nil
+	return len(zmd.Identifier) == 0 || len(zmd.Message) == 0
 }
