84f3bf126a8c381f0e4ff67ea751457b100cd09c
Felipe Nunes <fnap@cin.ufpe.br>
2024-10-17 14:37:24 -0300

    Fix livestream using third parties (#23)
    This pull requests repurposes a docker-compose and adds a new one. Also,
    makes required changes to use raw vision packets on `perception-ms`.
    
    - `docker-compose.metrics`: became `docker-compose.lab` with required
    services to run a game at the lab.
    - `docker-compose.prod`: same as `docker-compose.lab` and should be used
    in competition scenarios.

--- perception-ms/perception-brutus/perception/parameters/parameters.h
+++ perception-ms/perception-brutus/perception/parameters/parameters.h
@@ -9,7 +9,7 @@ namespace perception {
 constinit const auto pPerceptionPollerTimeoutMs
     = ::robocin::parameters::View<1>::asInt32(10 /*ms ~= 100Hz*/);
 // TODO(matheusvtna): retrieve the vision source from perception parameters.
-constinit const auto pUseTrackedDetectionAsSource = ::robocin::parameters::View<2>::asBool(true);
+constinit const auto pUseTrackedDetectionAsSource = ::robocin::parameters::View<2>::asBool(false);
 constinit const auto pRobotRadius = ::robocin::parameters::View<3>::asFloat(90);
 constinit const auto pRobotHeight = ::robocin::parameters::View<4>::asFloat(140);
 constinit const auto pDribblerWidth = ::robocin::parameters::View<5>::asFloat(60);
